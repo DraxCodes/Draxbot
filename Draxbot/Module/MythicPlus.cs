705453362125157050d25d3fe79f86188d1d522e
Draxis <joelparkinson@live.co.uk>
2018-01-19 17:52:15 +0000

    Added Highest Mythic+ as Embed Result.
    !mplus command will currently return the 3 highest ever dungeons completed by the player from raider.io. Data is displayed to the user as a discord embed.

--- Draxbot/Module/MythicPlus.cs
+++ Draxbot/Module/MythicPlus.cs
@@ -25,21 +25,17 @@ namespace Draxbot.Module
             string jsonDataUrl = ($"https://raider.io/api/v1/characters/profile?region=eu&realm={server}&name={s}&fields=mythic_plus_highest_level_runs%2Cgear%2Cguild%2Cmythic_plus_scores%2Cmythic_plus_ranks%2Cmythic_plus_recent_runs%2Cmythic_plus_best_runs%2Cmythic_plus_weekly_highest_level_runs%2Cprevious_mythic_plus_scores");
             string jsonData = null;
             jsonData = new WebClient().DownloadString(jsonDataUrl);
-            Character currentInfo = JsonConvert.DeserializeObject<Character>(jsonData);
+            Character mPlusData = JsonConvert.DeserializeObject<Character>(jsonData);
 
-            /*  var builder = new EmbedBuilder()
-                  .WithTitle($"{currentInfo.Name} | Mythic+ Breakdown")
-                  .AddField("Current Season", "")
-                  .WithColor(Color.Gold)
-                  .WithUrl(currentInfo.RaiderIO_URL)
-                  .WithColor(Color.DarkOrange)
-                  .WithFooter($"Powered by DraxBot & Raider.IO | {DateTime.Now.ToString("MMMM dd, yyyy")}"); */
+            var builderMP = new EmbedBuilder() { Title = $"Mythic+ Data For {s}", /*Description = $"Mythic+ Data Up To Date As Of {DateTime.UtcNow}",*/ Url = mPlusData.RaiderIO_URL};
+            builderMP.WithFooter($"Powered by Draxbot & Raider.IO | {DateTime.UtcNow}");
 
-            foreach (var d in currentInfo.HighestEverMythicPlus)
+            foreach (var d in mPlusData.HighestEverMythicPlus)
             {
-                await Context.Channel.SendMessageAsync($"{d.dungeon} Level: {d.mythic_level}");
+                builderMP.AddField($"[{d.short_name}]({d.url}) +{d.mythic_level}", null , true);
             }
-            
+
+            builderMP.Build(); await ReplyAsync("", false, builderMP);
         }
 
         private static string DownloadJsonData(string url)
